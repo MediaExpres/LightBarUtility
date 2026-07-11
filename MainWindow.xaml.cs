0582b8301706853e2b40d01fbb478f4dadb50ac3
Mihai <media.expres@gmail.com>
2026-07-11 12:29:43 +0300

    eliminat complet librăria WpfAppBar și funcția OnClosed, deoarece nu mai avem nevoie să înregistrăm nimic în Windows

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using WpfAppBar; 
 
 namespace LightBarUtility
 {
@@ -10,22 +9,18 @@ namespace LightBarUtility
         {
             InitializeComponent();
             
-            // Wait for the window handle to be ready before hooking into the OS
+            // Așteptăm ca fereastra să fie gata înainte să o mutăm
             this.SourceInitialized += MainWindow_SourceInitialized;
         }
 
-        // Added '?' to 'object' to resolve the CS8622 warning
         private void MainWindow_SourceInitialized(object? sender, EventArgs e)
         {
-            // Dock the window to the bottom edge of the screen
-            AppBarFunctions.SetAppBar(this, ABEdge.Bottom);
-        }
-
-        // Safely unregister the reserved space when the app closes
-        protected override void OnClosed(EventArgs e)
-        {
-            AppBarFunctions.SetAppBar(this, ABEdge.None);
-            base.OnClosed(e);
+            // Setăm lățimea barei pe toată lățimea ecranului
+            this.Width = SystemParameters.WorkArea.Width;
+            
+            // O poziționăm perfect în partea de jos (deasupra taskbar-ului)
+            this.Left = SystemParameters.WorkArea.Left;
+            this.Top = SystemParameters.WorkArea.Bottom - this.Height;
         }
     }
 }
