6fcfc36c2e7afcc75f314a62263928d95968892a
Mihai <media.expres@gmail.com>
2026-07-11 11:39:21 +0300

    1st build with error

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,31 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfAppBar; 
 
-namespace LightBarUtility;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace LightBarUtility
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Wait for the window handle to be ready before hooking into the OS
+            this.SourceInitialized += MainWindow_SourceInitialized;
+        }
+
+        // Added '?' to 'object' to resolve the CS8622 warning
+        private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+        {
+            // Dock the window to the bottom edge of the screen
+            AppBarFunctions.SetAppBar(this, ABEdge.Bottom);
+        }
+
+        // Safely unregister the reserved space when the app closes
+        protected override void OnClosed(EventArgs e)
+        {
+            AppBarFunctions.SetAppBar(this, ABEdge.None);
+            base.OnClosed(e);
+        }
     }
 }
