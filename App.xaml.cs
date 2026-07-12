268bed2df0ee4638887a34f6289f0dc604aa1c64
Mihai Pavelescu <media.expres@gmail.com>
2026-07-12 16:52:22 +0300

    Refactor single instance management by removing Mutex implementation and using process check for application startup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,42 +1,18 @@
 ﻿using System;
-using System.Threading;
+using System.Diagnostics;
 using System.Windows;
 
 namespace LightBarUtility;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-    private static Mutex? _mutex = null;
-
     protected override void OnStartup(StartupEventArgs e)
     {
-        const string appName = "LightBarUtility_UniqueInstance";
-        bool createdNew;
-
-        // Încearcă să creeze un Mutex cu un nume unic la nivel de sistem
-        _mutex = new Mutex(true, appName, out createdNew);
-
-        if (!createdNew)
+        if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
         {
-            // Dacă Mutex-ul există deja, închide aplicația instantaneu
             Environment.Exit(0);
         }
 
         base.OnStartup(e);
-    }
-
-    protected override void OnExit(ExitEventArgs e)
-    {
-        // Eliberează Mutex-ul la închiderea aplicației
-        if (_mutex != null)
-        {
-            _mutex.ReleaseMutex();
-            _mutex.Dispose();
-        }
-
-        base.OnExit(e);
     }
 }
