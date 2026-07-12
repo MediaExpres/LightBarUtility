6a387d37c31392962a178e1fa4d2a17a244a5f8b
Mihai Pavelescu <media.expres@gmail.com>
2026-07-12 16:26:55 +0300

    Implement mutex for single instance application management

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace LightBarUtility;
@@ -9,5 +9,35 @@ namespace LightBarUtility;
 /// </summary>
 public partial class App : Application
 {
-}
+    private static Mutex _mutex = null;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        const string appName = "LightBarUtility_UniqueInstance";
+        bool createdNew;
+
+        // Încearcă să creeze un Mutex cu un nume unic la nivel de sistem
+        _mutex = new Mutex(true, appName, out createdNew);
+
+        if (!createdNew)
+        {
+            // Dacă Mutex-ul există deja, închide aplicația imediat
+            Application.Current.Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        // Eliberează Mutex-ul la închiderea aplicației
+        if (_mutex != null)
+        {
+            _mutex.ReleaseMutex();
+            _mutex.Dispose();
+        }
+
+        base.OnExit(e);
+    }
+}
