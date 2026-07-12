cd262dfaa1e0541b2990ef2f22795d2418c8ff0f
Mihai Pavelescu <media.expres@gmail.com>
2026-07-12 16:56:57 +0300

    Refactor application startup to manage single instance using Mutex and remove StartupUri from App.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 
 namespace LightBarUtility;
 
 public partial class App : Application
 {
+    private static Mutex? _mutex = null;
+
     protected override void OnStartup(StartupEventArgs e)
     {
-        if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+        const string appName = "LightBarUtility_UniqueInstance";
+        bool createdNew;
+
+        _mutex = new Mutex(true, appName, out createdNew);
+
+        if (!createdNew)
         {
             Environment.Exit(0);
         }
 
         base.OnStartup(e);
+
+        // Deschidem fereastra manual doar dacă este prima instanță
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_mutex != null)
+        {
+            _mutex.ReleaseMutex();
+            _mutex.Dispose();
+        }
+
+        base.OnExit(e);
     }
 }
